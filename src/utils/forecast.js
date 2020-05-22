2c623f66b69b0e459569f00a76fba45cc7037d26
J Juan git <joshuajuan0010@gmail.com>
2020-05-23 00:58:37 +0530

    Small change of units of temp

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=b729ade91e79f253d7aa443f077d09ef&query=' +latitude+','+longitude+'&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=b729ade91e79f253d7aa443f077d09ef&query=' +latitude+','+longitude+'&units=c'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
